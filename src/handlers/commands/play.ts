bf029ecb31a2341a0c5caeea5803ddf244e96478
Josh Jones <folisdhc@gmail.com>
2024-12-20 11:55:25 -0500

    working on play; issue with followup messaging

--- src/handlers/commands/play.ts
+++ src/handlers/commands/play.ts
@@ -1,4 +1,14 @@
 import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonInteraction,
+  ButtonStyle,
+  CollectorFilter,
+  ComponentType,
+  InteractionResponse,
+  MessageComponentInteraction,
+} from "discord.js";
+import {
   InteractionController,
   SessionController,
 } from "..";
@@ -46,13 +56,118 @@ export const command: Command = {
           "ephemeral": true,
         });
       } else {
+        const drawButton: ButtonBuilder = new ButtonBuilder()
+          .setCustomId("draw")
+          .setLabel("Draw")
+          .setStyle(ButtonStyle.Success);
+        const standButton: ButtonBuilder = new ButtonBuilder()
+          .setCustomId("stand")
+          .setLabel("Stand")
+          .setStyle(ButtonStyle.Primary);
+        const cancelButton: ButtonBuilder = new ButtonBuilder()
+          .setCustomId("cancel")
+          .setLabel("Cancel")
+          .setStyle(ButtonStyle.Secondary);
+        const actionRow: ActionRowBuilder<ButtonBuilder> = new ActionRowBuilder<ButtonBuilder>()
+          .addComponents(
+            drawButton,
+            standButton,
+            cancelButton,
+          );
         const messageLines: string[] = [
           "You!",
         ];
-        await interaction.reply({
+        const actionResponse: InteractionResponse = await interaction.reply({
           "content": InteractionController.messageLinesToString(messageLines),
+          "components": [
+            actionRow,
+          ],
           "ephemeral": true,
         });
+        try {
+          const actionCollectorFilter: CollectorFilter<[MessageComponentInteraction]> = (i) =>
+            i.user.id === interaction.user.id && i.componentType === ComponentType.Button;
+          const actionButtonInteraction: ButtonInteraction = await actionResponse.awaitMessageComponent({
+            "filter": actionCollectorFilter,
+            "time": 60000, // 1 minute
+          }) as ButtonInteraction;
+          if (actionButtonInteraction.customId === "draw") {
+            const sandDrawDraw: ButtonBuilder = new ButtonBuilder()
+              .setCustomId("sandDrawDraw")
+              .setLabel("🟨 Draw")
+              .setStyle(ButtonStyle.Primary);
+            const sandDiscardDraw: ButtonBuilder = new ButtonBuilder()
+              .setCustomId("sandDiscardDraw")
+              .setLabel("🟨 Discard")
+              .setStyle(ButtonStyle.Primary);
+            const bloodDrawDraw: ButtonBuilder = new ButtonBuilder()
+              .setCustomId("bloodDrawDraw")
+              .setLabel("🟥 Draw")
+              .setStyle(ButtonStyle.Primary);
+            const bloodDiscardDraw: ButtonBuilder = new ButtonBuilder()
+              .setCustomId("bloodDiscardDraw")
+              .setLabel("🟥 Discard")
+              .setStyle(ButtonStyle.Primary);
+            const drawRow: ActionRowBuilder<ButtonBuilder> = new ActionRowBuilder<ButtonBuilder>()
+              .addComponents(
+                sandDrawDraw,
+                sandDiscardDraw,
+                bloodDrawDraw,
+                bloodDiscardDraw,
+                cancelButton,
+              );
+            const drawResponse: InteractionResponse = await actionButtonInteraction.reply({
+              "content": "DRAW",
+              "components": [
+                drawRow,
+              ],
+              "ephemeral": true,
+            });
+            await actionResponse.delete();
+            try {
+              const drawCollectorFilter: CollectorFilter<[MessageComponentInteraction]> = (i) =>
+                i.user.id === drawButtonInteraction.user.id && i.componentType === ComponentType.Button;
+              const drawButtonInteraction: ButtonInteraction = await drawResponse.awaitMessageComponent({
+                "filter": drawCollectorFilter,
+                "time": 60000, // 1 minute
+              }) as ButtonInteraction;
+              if (drawButtonInteraction.customId === "sandDrawDraw") {
+                await drawButtonInteraction.reply({
+                  "content": "sandDrawDraw",
+                  "ephemeral": true,
+                });
+              } else if (drawButtonInteraction.customId === "sandDiscardDraw") {
+                await actionButtonInteraction.reply({
+                  "content": "sandDiscardDraw",
+                  "ephemeral": true,
+                });
+              }
+              else if (drawButtonInteraction.customId === "bloodDrawDraw") {
+                await actionButtonInteraction.reply({
+                  "content": "bloodDrawDraw",
+                  "ephemeral": true,
+                });
+              } else if (drawButtonInteraction.customId === "bloodDiscardDraw") {
+                await actionButtonInteraction.reply({
+                  "content": "bloodDiscardDraw",
+                  "ephemeral": true,
+                });
+              }
+              await drawResponse.delete();
+            } catch (_: unknown) {
+              // On timeout
+              await drawResponse.delete();
+            }
+          } else if (actionButtonInteraction.customId === "stand") {
+            await actionButtonInteraction.reply({
+              "content": "STAND",
+              "ephemeral": true,
+            });
+          }
+        } catch (_: unknown) {
+          // On timeout
+          await actionResponse.delete();
+        }
       }
     }
   },
